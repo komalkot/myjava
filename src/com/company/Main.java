7629a0b984043f7389578c25ec139514c27c9f73
komal kotyal <komalkot@thoughtworks.com>
2020-10-02 15:10:07 +0530

    Komal | Voting qaulification check added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("Hello");
+        int shitalAge = 18, minimumVotingAgeInIndia = 18;
+        int komalAge = 19;
+        int votingAgeInAnotherCountry = minimumVotingAgeInIndia + 1;
+
+        System.out.println("Minimum voting age in India " + minimumVotingAgeInIndia);
+
+        printVotingQaulifcation("Shital", shitalAge, minimumVotingAgeInIndia);
+        printVotingQaulifcation("Komal", komalAge, votingAgeInAnotherCountry);
+    }
+
+    static void printVotingQaulifcation(String name, int personAge, int votingAge) {
+        String positiveStatement = " is Qualified for voting";
+        String negativeStatement = "is not qualified for voting";
+        if (personAge >= votingAge) {
+            System.out.println(name + positiveStatement);
+        } else {
+            System.out.println(name + negativeStatement);
+        }
     }
 }
